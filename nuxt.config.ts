9579ac81c2eb7565b7dba6a496bad7dbfed9700b
k-majick <studio@cyberleaf.pl>
2023-02-05 12:11:53 +0100

    package update + favicons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,6 +21,44 @@ export default defineNuxtConfig({
           rel: "stylesheet",
           href: "https://fonts.googleapis.com/css2?family=Fjalla+One:wght@200;400&display=swap",
         },
+        {
+          rel: 'manifest',
+          href: '/site.webmanifest',
+        },
+        {
+          rel: 'icon',
+          sizes: '192x192',
+          type: 'image/png',
+          href: '/android-chrome-192x192.png',
+        },
+        {
+          rel: 'icon',
+          sizes: '512x512',
+          type: 'image/png',
+          href: '/android-chrome-512x512.png',
+        },
+        {
+          rel: 'apple-touch-icon',
+          sizes: '180x180',
+          href: '/apple-touch-icon.png',
+        },
+        {
+          rel: 'icon',
+          sizes: '16x16',
+          type: 'image/png',
+          href: '/favicon-16x16.png',
+        },
+        {
+          rel: 'icon',
+          sizes: '32x32',
+          type: 'image/png',
+          href: '/favicon-32x32.png',
+        },
+        {
+          rel: 'icon',
+          type: 'image/x-icon',
+          href: '/favicon.ico',
+        },
       ],
     },
   },
@@ -32,8 +70,10 @@ export default defineNuxtConfig({
     },
   },
   runtimeConfig: {
-    apiUrl: process.env.API_URL || "http://localhost:3000",
-    appName: process.env.APP_NAME,
+    public: {
+      apiUrl: process.env.API_URL,
+      appName: process.env.APP_NAME,
+    },
   },
   vite: {
     plugins: [eslint()],
